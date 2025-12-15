64c1655486fd7257e7872c00cb2106e8c382c694
Senthil Nachimuthu <senthil@s8-app.med.utah.edu>
2025-12-15 13:29:37 -0700

    minimal python test 2 in index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,8 @@
-from fastapi import FastAPI, HTTPException, Query
-from fastapi.middleware.cors import CORSMiddleware
-import os
-import json
-
-app = FastAPI()
-
+# api/index.py
 from fastapi import FastAPI
- 
+
 app = FastAPI()
- 
+
 @app.get("/")
-def read_root():
-    return {"Python": "on Vercel"}
+def root():
+    return {"status": "ok", "framework": "fastapi", "runtime": "vercel-python"}
